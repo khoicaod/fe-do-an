9be99a5e011257fd4fece23e2aebede86e10dff4
Khoi Nguyen <khoinguyen.030901@gmail.com>
2023-10-02 22:32:36 +0700

    add not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function NotFound() {
-	return <div>NotFound</div>
+	return (
+		<div className='w-screen h-screen flex justify-center items-center'>
+			<img
+				className='w-1/2 h-1/2'
+				src={`https://static-00.iconduck.com/assets.00/404-page-not-found-illustration-2048x998-yjzeuy4v.png`}
+				alt='Not Found'
+			/>
+		</div>
+	)
 }
 
 export default NotFound
