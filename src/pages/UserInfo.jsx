c21a02cbe391e46e915ab05165d72b6eb7f0e77a
Khoi Nguyen <khoinguyen.030901@gmail.com>
2023-10-26 22:03:11 +0700

    add user management page

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -42,139 +42,143 @@ const UserInfo = () => {
 	})
 
 	return (
-		<div className='flex gap-4 items-start'>
-			<div className='w-4/12 shadow-lg shadow-gray-500 p-4 rounded-lg flex flex-col'>
-				<h1>Profile Image</h1>
-				<div className='w-full flex justify-center my-8'>
-					<img
-						className='w-40 h-40 rounded-full'
-						src={`https://ui-avatars.com/api/?name=${myInfo.fullName}`}
-						alt=''
-					/>
-				</div>
-				<button className='bg-blue-500 rounded-lg shadow-lg shadow-gray-500 py-2 px-4 font-semibold text-white'>
-					Change Other Image
-				</button>
-			</div>
-			<div className='w-8/12 shadow-lg shadow-gray-500 p-4 rounded-lg'>
-				<h1>Account Detail</h1>
-				<form
-					onSubmit={formik.handleSubmit}
-					className='flex flex-col gap-4 items-center justify-center grow px-10 my-8'>
-					<div className='w-full'>
-						<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
-							<AiOutlineUser />
-							<input
-								disabled
-								onChange={formik.handleChange}
-								value={formik.values.username}
-								placeholder='Username'
-								name='username'
-								className='focus:outline-none w-full disabled:text-gray-400 cursor-not-allowed'
-								type='text'
-							/>
-						</div>
-						{formik.touched.username && formik.errors.username ? (
-							<span className='text-red-500'>{formik.errors.username}</span>
-						) : null}
+		<div className='flex flex-col gap-8'>
+			<h1 className='text-4xl font-semibold'>User Information</h1>
+
+			<div className='flex gap-4 items-start'>
+				<div className='w-4/12 shadow-lg shadow-gray-500 p-4 rounded-lg flex flex-col'>
+					<h1>Profile Image</h1>
+					<div className='w-full flex justify-center my-8'>
+						<img
+							className='w-40 h-40 rounded-full'
+							src={`https://ui-avatars.com/api/?name=${myInfo.fullName}`}
+							alt=''
+						/>
 					</div>
-					<div className='w-full flex gap-4'>
-						<div className='w-6/12 flex items-center text-xl gap-4 border px-4 py-1'>
-							<AiFillLock />
-							<input
-								onChange={formik.handleChange}
-								value={formik.values.currentPassword}
-								placeholder='Current Password'
-								name='currentPassword'
-								className='focus:outline-none w-full'
-								type='password'
-							/>
-						</div>
-						<div className='w-6/12 flex items-center text-xl gap-4 border px-4 py-1'>
-							<AiFillLock />
-							<input
-								disabled={formik.values.currentPassword !== '' ? false : true}
-								onChange={formik.handleChange}
-								value={formik.values.newPassword}
-								placeholder='New Password'
-								name='newPassword'
-								className={`focus:outline-none w-full ${
-									formik.values.currentPassword !== '' ? '' : 'cursor-not-allowed'
-								}`}
-								type='password'
-							/>
-						</div>
-					</div>
-					<div className='w-full'>
-						<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
-							<AiOutlineMail />
-							<input
-								onChange={formik.handleChange}
-								value={formik.values.email}
-								placeholder='Email'
-								name='email'
-								className='focus:outline-none w-full'
-								type='email'
-							/>
-						</div>
-						{formik.touched.email && formik.errors.email ? (
-							<span className='text-red-500'>{formik.errors.email}</span>
-						) : null}
-					</div>
-					<div className='w-full'>
-						<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
-							<FaRegAddressBook />
-							<input
-								onChange={formik.handleChange}
-								value={formik.values.address}
-								placeholder='Address'
-								name='address'
-								className='focus:outline-none w-full'
-								type='text'
-							/>
-						</div>
-						{formik.touched.address && formik.errors.address ? (
-							<span className='text-red-500'>{formik.errors.address}</span>
-						) : null}
-					</div>
-					<div className='w-full'>
-						<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
-							<BiRename />
-							<input
-								onChange={formik.handleChange}
-								value={formik.values.fullName}
-								placeholder='Full name'
-								name='fullName'
-								className='focus:outline-none w-full'
-								type='text'
-							/>
-						</div>
-						{formik.touched.fullName && formik.errors.fullName ? (
-							<span className='text-red-500'>{formik.errors.fullName}</span>
-						) : null}
-					</div>
-					<div className='w-full'>
-						<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
-							<AiFillPhone />
-							<input
-								onChange={formik.handleChange}
-								value={formik.values.phoneNumber}
-								placeholder='Phone number'
-								name='phoneNumber'
-								className='focus:outline-none w-full'
-								type='text'
-							/>
-						</div>
-						{formik.touched.phoneNumber && formik.errors.phoneNumber ? (
-							<span className='text-red-500'>{formik.errors.phoneNumber}</span>
-						) : null}
-					</div>
-					<button
-						type='submit'
-						className='bg-blue-500 w-full px-4 py-2 rounded-lg shadow-lg shadow-gray-500 font-semibold text-white'>
-						Update Info
+					<button className='bg-blue-500 rounded-lg shadow-lg shadow-gray-500 py-2 px-4 font-semibold text-white'>
+						Change Other Image
 					</button>
-				</form>
+				</div>
+				<div className='w-8/12 shadow-lg shadow-gray-500 p-4 rounded-lg'>
+					<h1>Account Detail</h1>
+					<form
+						onSubmit={formik.handleSubmit}
+						className='flex flex-col gap-4 items-center justify-center grow px-10 my-8'>
+						<div className='w-full'>
+							<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
+								<AiOutlineUser />
+								<input
+									disabled
+									onChange={formik.handleChange}
+									value={formik.values.username}
+									placeholder='Username'
+									name='username'
+									className='focus:outline-none w-full disabled:text-gray-400 cursor-not-allowed'
+									type='text'
+								/>
+							</div>
+							{formik.touched.username && formik.errors.username ? (
+								<span className='text-red-500'>{formik.errors.username}</span>
+							) : null}
+						</div>
+						<div className='w-full flex gap-4'>
+							<div className='w-6/12 flex items-center text-xl gap-4 border px-4 py-1'>
+								<AiFillLock />
+								<input
+									onChange={formik.handleChange}
+									value={formik.values.currentPassword}
+									placeholder='Current Password'
+									name='currentPassword'
+									className='focus:outline-none w-full'
+									type='password'
+								/>
+							</div>
+							<div className='w-6/12 flex items-center text-xl gap-4 border px-4 py-1'>
+								<AiFillLock />
+								<input
+									disabled={formik.values.currentPassword !== '' ? false : true}
+									onChange={formik.handleChange}
+									value={formik.values.newPassword}
+									placeholder='New Password'
+									name='newPassword'
+									className={`focus:outline-none w-full ${
+										formik.values.currentPassword !== '' ? '' : 'cursor-not-allowed'
+									}`}
+									type='password'
+								/>
+							</div>
+						</div>
+						<div className='w-full'>
+							<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
+								<AiOutlineMail />
+								<input
+									onChange={formik.handleChange}
+									value={formik.values.email}
+									placeholder='Email'
+									name='email'
+									className='focus:outline-none w-full'
+									type='email'
+								/>
+							</div>
+							{formik.touched.email && formik.errors.email ? (
+								<span className='text-red-500'>{formik.errors.email}</span>
+							) : null}
+						</div>
+						<div className='w-full'>
+							<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
+								<FaRegAddressBook />
+								<input
+									onChange={formik.handleChange}
+									value={formik.values.address}
+									placeholder='Address'
+									name='address'
+									className='focus:outline-none w-full'
+									type='text'
+								/>
+							</div>
+							{formik.touched.address && formik.errors.address ? (
+								<span className='text-red-500'>{formik.errors.address}</span>
+							) : null}
+						</div>
+						<div className='w-full'>
+							<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
+								<BiRename />
+								<input
+									onChange={formik.handleChange}
+									value={formik.values.fullName}
+									placeholder='Full name'
+									name='fullName'
+									className='focus:outline-none w-full'
+									type='text'
+								/>
+							</div>
+							{formik.touched.fullName && formik.errors.fullName ? (
+								<span className='text-red-500'>{formik.errors.fullName}</span>
+							) : null}
+						</div>
+						<div className='w-full'>
+							<div className='w-full flex items-center text-xl gap-4 border px-4 py-1'>
+								<AiFillPhone />
+								<input
+									onChange={formik.handleChange}
+									value={formik.values.phoneNumber}
+									placeholder='Phone number'
+									name='phoneNumber'
+									className='focus:outline-none w-full'
+									type='text'
+								/>
+							</div>
+							{formik.touched.phoneNumber && formik.errors.phoneNumber ? (
+								<span className='text-red-500'>{formik.errors.phoneNumber}</span>
+							) : null}
+						</div>
+						<button
+							type='submit'
+							className='bg-blue-500 w-full px-4 py-2 rounded-lg shadow-lg shadow-gray-500 font-semibold text-white'>
+							Update Info
+						</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	)
--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import { Table } from 'antd'
+import React, { useEffect } from 'react'
+import { getAllUserAction } from '../redux/actions/userAction'
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai'
+import { useDispatch, useSelector } from 'react-redux'
 
 const UserManagement = () => {
-	return <div>UserManagement</div>
+	const dispatch = useDispatch()
+	const { users } = useSelector((state) => state.userReducer)
+
+	useEffect(() => {
+		dispatch(getAllUserAction())
+	}, [])
+
+	const columns = [
+		{
+			title: 'Username',
+			dataIndex: 'username',
+		},
+		{
+			title: 'Role',
+			dataIndex: 'role',
+		},
+		{
+			title: 'Full Name',
+			dataIndex: 'fullName',
+		},
+		{
+			title: 'Phone Number',
+			dataIndex: 'phoneNumber',
+		},
+		{
+			title: 'Email',
+			dataIndex: 'email',
+		},
+		{
+			title: 'Address',
+			dataIndex: 'address',
+		},
+		{
+			title: 'Delete / Edit',
+			render(text, record) {
+				return (
+					<div className='flex gap-4 justify-center'>
+						<span
+							onClick={() => {
+								if (window.confirm('Bạn muốn xóa phim này ?') === true) {
+									// dispatch(actionXoaPhim(record.maPhim))
+								}
+							}}
+							className='text-red-500 cursor-pointer text-2xl'>
+							<AiFillDelete />
+						</span>
+						<span
+							// onClick={() => navigate(`edit-film/${record.maPhim}`)}
+							className='text-blue-500 cursor-pointer text-2xl'>
+							<AiFillEdit />
+						</span>
+					</div>
+				)
+			},
+		},
+	]
+
+	return (
+		<div className='flex flex-col gap-8'>
+			<h1 className='text-4xl font-semibold'>User Management</h1>
+			<Table
+				rowKey={(record) => {
+					return record.pk
+				}}
+				bordered
+				columns={columns}
+				dataSource={users}
+			/>
+		</div>
+	)
 }
 
 export default UserManagement
